1ce05595290e09ac456f345b7477044d172f6e27
Mateus Marinheiro <mateusmarinheiro2005@gmail.com>
2025-12-16 22:55:15 -0300

    feat: adicionar novos campos de endereço ao modelo Empresa

---
+++ sis-frontend/prisma/migrations/20251217015450_add_empresa_address_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE `tab_empresa` ADD COLUMN `cidade` VARCHAR(191) NULL,
+    ADD COLUMN `complemento` VARCHAR(191) NULL,
+    ADD COLUMN `estado` VARCHAR(191) NULL,
+    ADD COLUMN `logradouro` VARCHAR(191) NULL,
+    ADD COLUMN `numero` VARCHAR(191) NULL,
+    ADD COLUMN `pais` VARCHAR(191) NULL;
